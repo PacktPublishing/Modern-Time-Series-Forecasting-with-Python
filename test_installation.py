e983dc809ef38d4190f28132d6b5a953d6aa1347
Manu Joseph <manujosephv@gmail.com>
2021-11-08 17:22:09 +0530

    removed all notebooks --to change the folder structure

---
+++ test_installation.py
@@ -0,0 +1,28 @@
+import numpy as np
+x = np.arange(5)
+import pandas as pd
+df = pd.DataFrame(x)
+import torch
+d = torch.zeros(3)
+print(
+    f"GPU: {torch.cuda.is_available()} | # of GPU: {torch.cuda.device_count()}| Default GPU Name: {torch.cuda.get_device_name(0)}"
+)
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+import plotly.io as pio
+import pandas as pd
+from tqdm.autonotebook import tqdm
+import missingno as msno
+import statsmodels.api as sm
+from sklearn.ensemble import RandomForestRegressor
+import xgboost
+import lightgbm
+import catboost
+import seaborn
+import pmdarima as pm
+from darts import TimeSeries
+from darts.models import NaiveSeasonal
+from prophet import Prophet
+import pytorch_lightning as pl
+print("All Libraries imported without errors!")
