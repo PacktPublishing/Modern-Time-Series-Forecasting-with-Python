50ef00ebd384ee027423f425fd9c9278f3099224
Manu Joseph <manujosephv@gmail.com>
2022-07-15 05:35:24 +0530

    added chapter 12 and 13

---
+++ src/dl/attention.py
@@ -0,0 +1,176 @@
+from abc import ABCMeta, abstractmethod
+import logging
+import math
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch import Tensor
+
+
+class Attention(nn.Module, metaclass=ABCMeta):
+    def __init__(self, encoder_dim: int, decoder_dim: int):
+        super().__init__()
+        self.encoder_dim = encoder_dim
+        self.decoder_dim = decoder_dim
+
+    @abstractmethod
+    def _get_scores(
+        self,
+        q: torch.Tensor,  # [batch_size, decoder_dim]
+        v: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ) -> torch.Tensor:  # [batch_size, seq_length]
+        pass
+
+    def forward(
+        self,
+        query: torch.Tensor,  # [batch_size, decoder_dim]
+        values: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ):
+        if query.ndim==2:
+            query = query.unsqueeze(1)
+        elif query.ndim==3:
+            assert query.shape[1]==1, "If `query` is a 3-D tensor should have shape [batch_size, 1, decoder_dim]"
+        seq_length = values.shape[1]
+        scores = self._get_scores(query, values)  # [batch_size, seq_length]
+        assert scores.size(1) == seq_length
+        weights = torch.nn.functional.softmax(scores, dim=-1)
+        return (values * weights.unsqueeze(-1)).sum(dim=1)  # [batch_size, encoder_dim]
+        # return torch.bmm(values.transpose(1,2), weights.unsqueeze(-1)).squeeze()  # [batch_size, encoder_dim]
+
+
+class DotProductAttention(Attention):
+    def __init__(self, hidden_dim: int, scaled: bool = True):
+        super().__init__(hidden_dim, hidden_dim)
+        if scaled:
+            self.scaling = math.sqrt(hidden_dim)
+        else:
+            self.scaling = 1.0
+
+    def _get_scores(
+        self,
+        q: torch.Tensor,  # [batch_size, decoder_dim]
+        v: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ) -> torch.Tensor:  # [batch_size, seq_length]
+        scores = q @ v.transpose(1, 2)  # [batch_size, seq_length]
+        return scores.squeeze(1) / self.scaling  # [batch_size, seq_length]
+
+
+class GeneralAttention(Attention):
+    def __init__(self, encoder_dim: int, decoder_dim: int):
+        super().__init__(encoder_dim, decoder_dim)
+        self.W = torch.nn.Parameter(
+            torch.FloatTensor(self.decoder_dim, self.encoder_dim).uniform_(-0.1, 0.1)
+        )
+
+    def _get_scores(
+        self,
+        q: torch.Tensor,  # [batch_size, decoder_dim]
+        v: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ) -> torch.Tensor:  # [batch_size, seq_length]
+        scores = (q @ self.W) @ v.transpose(1, 2)  # [batch_size, seq_length]
+        return scores.squeeze(1)  # [batch_size, seq_length]
+
+
+class AdditiveAttention(Attention):
+    def __init__(self, encoder_dim: int, decoder_dim: int):
+        super().__init__(encoder_dim, decoder_dim)
+        self.v = torch.nn.Parameter(torch.FloatTensor(self.decoder_dim).uniform_(-0.1, 0.1))
+        self.W_q = torch.nn.Linear(self.decoder_dim, self.decoder_dim)
+        self.W_v = torch.nn.Linear(self.encoder_dim, self.decoder_dim)
+
+    def _get_scores(
+        self,
+        q: torch.Tensor,  # [batch_size, decoder_dim]
+        v: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ) -> torch.Tensor:  # [batch_size, seq_length]
+        q = q.repeat(1, v.size(1), 1)  # [batch_size, seq_length, decoder_dim]
+        scores = self.W_q(q) + self.W_v(v)  # [batch_size, seq_length, decoder_dim]
+        return torch.tanh(scores) @ self.v  # [batch_size, seq_length]
+
+
+# Also referred to as Concat attention by Luong et al.
+class ConcatAttention(Attention):
+    def __init__(self, hidden_dim: int):
+        super().__init__(hidden_dim, hidden_dim)
+        self.v = torch.nn.Parameter(
+            torch.FloatTensor(hidden_dim).uniform_(-0.1, 0.1)
+        )
+        self.W = torch.nn.Linear(2*hidden_dim, hidden_dim)
+
+    def _get_scores(
+        self,
+        q: torch.Tensor,  # [batch_size, decoder_dim]
+        v: torch.Tensor,  # [batch_size, seq_length, encoder_dim]
+    ) -> torch.Tensor:  # [batch_size, seq_length]
+        q = q.repeat(1, v.size(1), 1)  # [batch_size, seq_length, decoder_dim]
+        scores = self.W(
+            torch.cat([q, v], dim=-1)
+        )  # [batch_size, seq_length, decoder_dim]
+        return torch.tanh(scores) @ self.v  # [batch_size, seq_length]
+
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, input_dim: int, attn_dim: int):
+        super(ScaledDotProductAttention, self).__init__()
+        self.input_dim = input_dim
+        self.attn_dim = attn_dim
+        self.scaling = math.sqrt(input_dim)
+        self.W_q = nn.Linear(input_dim, attn_dim, bias=False)
+        self.W_k = nn.Linear(input_dim, attn_dim, bias=False)
+        self.W_v = nn.Linear(input_dim, attn_dim, bias=False)
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        # DIM: q, k, v --> [batch_size, seq_length, hidden_dim]
+        q = self.W_q(q) # [batch_size, seq_length, attn_dim]
+        k = self.W_k(k) # [batch_size, seq_length, attn_dim]
+        v = self.W_v(v) # [batch_size, seq_length, attn_dim]
+        attn_energies = q.bmm(k.transpose(1, 2))  # [batch_size, seq_length, seq_length]
+        attn_energies = attn_energies / self.scaling  # [batch_size, seq_length, seq_length]
+        attn_weights = F.softmax(attn_energies, dim=-1)  # [batch_size, seq_length, seq_length]
+        return torch.bmm(
+            attn_weights, v
+        )  # v(k,q) = v * p(a(k,q)) and sum across attention dim to get [batch_size, seq_length, attn_dim]
+
+
+# input_dim = 10
+# attn_dim = 10
+# # q = torch.randn(100, 5, input_dim)
+# q = torch.randn(100, 7, input_dim)
+# k = torch.randn(100, 7, input_dim)
+# v = torch.randn(100, 7, input_dim)
+
+# for mode in ["dot", "scaled_dot", "general", "concat", "additive"]:
+#     attn = Attention(mode, input_dim)
+#     att_v = attn(q, k, v)
+#     print(att_v.shape)
+
+
+# hidden_dim = 15
+# seq_len = 20
+# bz = 1
+
+# q = torch.randn(bz, hidden_dim)
+# v = torch.randn(bz, seq_len, hidden_dim)
+
+# attn = ConcatAttention(hidden_dim)
+# att = attn(q, v)
+# print(att.shape)
+# attn = DotProductAttention(hidden_dim, scaled=False)
+# att = attn(q, v)
+# print(att.shape)
+
+
+# encoder_dim = 10
+# decoder_dim = 15
+# seq_len = 20
+# bz = 1
+# q = torch.randn(bz, decoder_dim)
+# v = torch.randn(bz, seq_len, encoder_dim)
+# attn = AdditiveAttention(encoder_dim, decoder_dim)
+# att = attn(q, v)
+# print(att.shape)
+# attn = GeneralAttention(encoder_dim, decoder_dim)
+# att = attn(q, v)
+# print(att.shape)
--- src/dl/models.py
+++ src/dl/models.py
@@ -14,6 +14,22 @@ from omegaconf import DictConfig, OmegaConf
 from torch import Tensor
 import enum
 
+from src.dl.attention import GeneralAttention, AdditiveAttention, DotProductAttention, ConcatAttention
+# from attention import (
+#     GeneralAttention,
+#     AdditiveAttention,
+#     DotProductAttention,
+#     ConcatAttention,
+# )
+
+ATTENTION_TYPES = {
+    "dot": DotProductAttention,
+    "scaled_dot": DotProductAttention,
+    "general": GeneralAttention,
+    "additive": AdditiveAttention,
+    "concat": ConcatAttention,
+}
+
 
 @dataclass
 class SingleStepRNNConfig:
@@ -219,6 +235,7 @@ class SingleStepRNNModel(BaseModel):
 @dataclass
 class RNNConfig:
     """Configuration for RNN"""
+
     input_size: int
     hidden_size: int
     num_layers: int
@@ -258,9 +275,14 @@ class Seq2SeqConfig:
             "RNN",
             "FC",
         ], f"{self.decoder_type} is not a valid RNN type"
-        if self.decoder_type=="FC":
-            assert "window_size" in self.decoder_params, "window_size is required for FC decoder"
-            assert "horizon" in self.decoder_params, "horizon is required for FC decoder"
+        if self.decoder_type == "FC":
+            assert (
+                "window_size" in self.decoder_params
+            ), "window_size is required for FC decoder"
+            assert (
+                "horizon" in self.decoder_params
+            ), "horizon is required for FC decoder"
+
 
 class Seq2SeqModel(BaseModel):
     def __init__(
@@ -271,7 +293,13 @@ class Seq2SeqModel(BaseModel):
         super().__init__(config)
 
     def _build_network(self):
-        bi_directional_multiplier = 2 if self.hparams.encoder_params["bidirectional"] else 1
+        enc_bi_directional_multiplier = (
+            2 if self.hparams.encoder_params["bidirectional"] else 1
+        )
+        if self.hparams.decoder_type != "FC":
+            dec_bi_directional_multiplier = (
+                2 if self.hparams.decoder_params["bidirectional"] else 1
+            )
         if self.hparams.encoder_type == "RNN":
             self.encoder = nn.RNN(
                 **self.hparams.encoder_params,
@@ -307,24 +335,34 @@ class Seq2SeqModel(BaseModel):
         elif self.hparams.decoder_type == "FC":
             if self.hparams.decoder_use_all_hidden:
                 self.decoder = nn.Linear(
-                    self.hparams.encoder_params.hidden_size * bi_directional_multiplier * self.hparams.decoder_params.window_size, self.hparams.decoder_params.horizon
+                    self.hparams.encoder_params.hidden_size
+                    * enc_bi_directional_multiplier
+                    * self.hparams.decoder_params.window_size,
+                    self.hparams.decoder_params.horizon,
                 )
             else:
                 self.decoder = nn.Linear(
-                    self.hparams.encoder_params.hidden_size*bi_directional_multiplier, self.hparams.decoder_params.horizon
+                    self.hparams.encoder_params.hidden_size
+                    * enc_bi_directional_multiplier,
+                    self.hparams.decoder_params.horizon,
                 )
-        if self.hparams.decoder_type!="FC":
-            self.fc = nn.Linear(self.hparams.decoder_params.hidden_size*bi_directional_multiplier, 1)
+        if self.hparams.decoder_type != "FC":
+            self.fc = nn.Linear(
+                self.hparams.decoder_params.hidden_size * dec_bi_directional_multiplier,
+                1,
+            )
 
     def forward(self, batch: Tuple[torch.Tensor, torch.Tensor]):
         x, y = batch
-        o, h = self.encoder(x) # --> (batch_size, seq_len, hidden_size) , (num_layers, batch_size, hidden_size) (hidden size*2 and num_layers*2 if bidirectional)
+        o, h = self.encoder(
+            x
+        )  # --> (batch_size, seq_len, hidden_size) , (num_layers, batch_size, hidden_size) (hidden size*2 and num_layers*2 if bidirectional)
         if self.hparams.decoder_type == "FC":
             if self.hparams.decoder_use_all_hidden:
                 y_hat = self.decoder(o.reshape(o.size(0), -1)).unsqueeze(-1)
             else:
-                y_hat = self.decoder(o[:,-1,:]).unsqueeze(-1)
-                
+                y_hat = self.decoder(o[:, -1, :]).unsqueeze(-1)
+
         else:
             # Loop to generate target
             y_hat = torch.zeros_like(y, device=y.device)
@@ -333,7 +371,7 @@ class Seq2SeqModel(BaseModel):
                 out, h = self.decoder(dec_input, h)
                 out = self.fc(out)
                 y_hat[:, i, :] = out.squeeze(1)
-                #decide if we are going to use teacher forcing or not
+                # decide if we are going to use teacher forcing or not
                 teacher_force = random.random() < self.hparams.teacher_forcing_ratio
                 if teacher_force:
                     dec_input = y[:, i, :].unsqueeze(1)
@@ -341,13 +379,177 @@ class Seq2SeqModel(BaseModel):
                     dec_input = out
         return y_hat, y
 
+    def predict(
+        self, batch: Tuple[torch.Tensor, torch.Tensor], ret_model_output: bool = False
+    ):
+        with torch.no_grad():
+            y_hat, _ = self.forward(batch)
+        return y_hat
 
-        # # x --> (batch_size, seq_len, input_size), y--> (batch_size, seq_len, 1)
-        # x, _ = self.rnn(x)  # --> (batch_size, seq_len, hidden_size)
-        # x = self.fc(x)  # --> (batch_size, seq_len, 1)
-        # # shifting the input by one and concatenating with the output to get the target
-        # y = torch.cat([x[:, 1:, :], y], dim=1)  # --> (batch_size, seq_len, 1)
-        # return x, y
+
+@dataclass
+class Seq2SeqwAttnConfig:
+    """Configuration for RNN"""
+
+    encoder_type: str
+    decoder_type: str
+    encoder_params: Dict[str, Any]
+    decoder_params: Dict[str, Any]
+    teacher_forcing_ratio: float = 0.0
+    learning_rate: float = field(default=1e-3)
+    optimizer_params: Dict = field(default_factory=dict)
+    lr_scheduler: Optional[str] = field(default=None)
+    lr_scheduler_params: Dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        self.encoder_type = self.encoder_type.upper()
+        self.decoder_type = self.decoder_type.upper()
+        if isinstance(self.encoder_params, RNNConfig):
+            self.encoder_params = self.encoder_params.__dict__
+        if isinstance(self.decoder_params, RNNConfig):
+            self.decoder_params = self.decoder_params.__dict__
+        assert self.encoder_type in [
+            "LSTM",
+            "GRU",
+            "RNN",
+        ], f"{self.encoder_type} is not a valid RNN type"
+        assert self.decoder_type in [
+            "LSTM",
+            "GRU",
+            "RNN",
+        ], f"{self.decoder_type} is not a valid RNN type"
+        effective_encoder_hidden_size = (
+            self.encoder_params["hidden_size"]
+            * (2 if self.encoder_params["bidirectional"] else 1)
+        )
+        assert self.decoder_params["input_size"] == self.encoder_params['input_size'] + effective_encoder_hidden_size, (
+            f"Encoder input size {self.encoder_params['input_size']} + "
+            f"Encoder hidden size (*2 if bi directional) {effective_encoder_hidden_size} != "
+            f"Decoder input size {self.decoder_params['input_size']}"
+        )
+
+
+class Seq2SeqwAttnModel(BaseModel):
+    def __init__(
+        self,
+        attention_type: str,
+        config: DictConfig,
+        **kwargs,
+    ):
+        assert (
+            attention_type.lower() in ATTENTION_TYPES.keys()
+        ), f"{attention_type} is not a valid attention type"
+        self.attention_type = attention_type.lower()
+        super().__init__(config)
+
+    def _build_network(self):
+        enc_bi_directional_multiplier = (
+            2 if self.hparams.encoder_params["bidirectional"] else 1
+        )
+        dec_bi_directional_multiplier = (
+            2 if self.hparams.decoder_params["bidirectional"] else 1
+        )
+        self.attention = ATTENTION_TYPES[self.attention_type]
+        attn_params = dict()
+        if issubclass(self.attention, ConcatAttention) or issubclass(
+            self.attention, DotProductAttention
+        ):
+            assert (
+                self.hparams.encoder_params["hidden_size"]
+                * enc_bi_directional_multiplier
+                == self.hparams.decoder_params["hidden_size"]
+                * dec_bi_directional_multiplier
+            ), "Hidden size*D, where D=2 for bi directional and 1 otherwise, of encoder and decoder must be equal"
+            attn_params = {
+                "hidden_dim": self.hparams.encoder_params.hidden_size
+                * enc_bi_directional_multiplier
+            }
+            if issubclass(self.attention, DotProductAttention):
+                if "scale" in self.attention_type:
+                    attn_params["scaled"] = True
+                else:
+                    attn_params["scaled"] = False
+        else:
+            attn_params = {
+                "encoder_dim": self.hparams.encoder_params.hidden_size
+                * enc_bi_directional_multiplier,
+                "decoder_dim": self.hparams.decoder_params.hidden_size
+                * dec_bi_directional_multiplier,
+            }
+        if "scale" in self.attention_type:
+            if issubclass(self.attention, DotProductAttention):
+                attn_params["scaled"] = True
+        self.attention = self.attention(**attn_params)
+
+        if self.hparams.encoder_type == "RNN":
+            self.encoder = nn.RNN(
+                **self.hparams.encoder_params,
+                batch_first=True,
+            )
+        elif self.hparams.encoder_type == "LSTM":
+            self.encoder = nn.LSTM(
+                **self.hparams.encoder_params,
+                batch_first=True,
+            )
+        elif self.hparams.encoder_type == "GRU":
+            self.encoder = nn.GRU(
+                **self.hparams.encoder_params,
+                batch_first=True,
+            )
+        else:
+            raise ValueError("Invalid RNN type")
+        if self.hparams.decoder_type == "RNN":
+            self.decoder = nn.RNN(
+                **self.hparams.decoder_params,
+                batch_first=True,
+            )
+        elif self.hparams.decoder_type == "LSTM":
+            self.decoder = nn.LSTM(
+                **self.hparams.decoder_params,
+                batch_first=True,
+            )
+        elif self.hparams.decoder_type == "GRU":
+            self.decoder = nn.GRU(
+                **self.hparams.decoder_params,
+                batch_first=True,
+            )
+        self.fc = nn.Linear(
+            self.hparams.decoder_params.hidden_size * dec_bi_directional_multiplier, 1
+        )
+
+    def _get_top_layer_hidden_state(self, hidden_state):
+        if self.hparams.encoder_type == "LSTM":
+            hidden_state, _ = hidden_state
+        if self.hparams.encoder_params["bidirectional"]:
+            # concatenating the forward and backward hidden states
+            return torch.cat((hidden_state[-1, :, :], hidden_state[-2, :, :]), dim=-1)
+        else:
+            return hidden_state[-1, :, :]
+
+    def forward(self, batch: Tuple[torch.Tensor, torch.Tensor]):
+        x, y = batch
+        o, h = self.encoder(
+            x
+        )  # --> (batch_size, seq_len, hidden_size) , (num_layers, batch_size, hidden_size) (hidden size*2 and num_layers*2 if bidirectional)
+        # Loop to generate target
+        y_hat = torch.zeros_like(y, device=y.device)
+        dec_input = x[:, -1:, :]
+        for i in range(y.size(1)):
+            top_h = self._get_top_layer_hidden_state(h) # --> (batch_size, hidden_size*2 if bidirectional)
+            context = self.attention(
+                top_h.unsqueeze(1), o
+            )  # --> (batch_size, hidden_size)
+            dec_input = torch.cat((dec_input, context.unsqueeze(1)), dim=-1)
+            out, h = self.decoder(dec_input, h)
+            out = self.fc(out)
+            y_hat[:, i, :] = out.squeeze(1)
+            # decide if we are going to use teacher forcing or not
+            teacher_force = random.random() < self.hparams.teacher_forcing_ratio
+            if teacher_force:
+                dec_input = y[:, i, :].unsqueeze(1)
+            else:
+                dec_input = out
+        return y_hat, y
 
     def predict(
         self, batch: Tuple[torch.Tensor, torch.Tensor], ret_model_output: bool = False
@@ -355,3 +557,32 @@ class Seq2SeqModel(BaseModel):
         with torch.no_grad():
             y_hat, _ = self.forward(batch)
         return y_hat
+
+
+# x = torch.rand(256, 48, 1)
+# y = torch.rand(256, 1, 1)
+# encoder_config = RNNConfig(
+#     input_size=1,
+#     hidden_size=128,
+#     num_layers=3,
+#     bidirectional=True,
+# ).__dict__
+# decoder_config = RNNConfig(
+#     input_size=1 + 128*2,
+#     hidden_size=128,
+#     num_layers=3,
+#     bidirectional=True,
+# ).__dict__
+# rnn2rnn_config = Seq2SeqwAttnConfig(
+#     encoder_type="LSTM",
+#     decoder_type="LSTM",
+#     encoder_params=encoder_config,
+#     decoder_params=decoder_config,
+#     learning_rate=1e-3,
+# )
+
+# for at in ATTENTION_TYPES.keys():
+#     print(at)
+#     model = Seq2SeqwAttnModel(at, rnn2rnn_config)
+#     yhat, y = model((x, y))
+#     print(yhat.shape)
